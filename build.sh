6facfdd0fa2f27fe57e03e4336357991ad7191d1
Nviera <yuvarajsomavamshi@gmail.com>
2021-06-25 10:52:52 +0530

    Initial commit

---
+++ build.sh
@@ -0,0 +1,10 @@
+start=`date +%s`
+npm run prettify
+docker-compose -f docker-compose.yml build
+
+docker tag breathalyzer_api:traciex blockchain.azurecr.io/breathalyzer_api:traciex
+docker push blockchain.azurecr.io/breathalyzer_api:traciex
+
+end=`date +%s`
+runtime=$((end-start))
+echo "$runtime"
