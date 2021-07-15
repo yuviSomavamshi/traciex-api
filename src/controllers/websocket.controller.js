d28ac0d45c44e9a3bd48105c39e0434752945e47
Yuvaraj K <yuvarajsomavamshi@gmail.com>
2021-07-16 00:50:07 +0530

    Web timer fix

--- src/controllers/websocket.controller.js
+++ src/controllers/websocket.controller.js
@@ -4,16 +4,13 @@ const router = express.Router();
 const authorize = require("../_middleware/authorize");
 const Role = require("../_helpers/role");
 
-const clients = {};
-const senders = {};
-
 var io;
 
 router.post("/register", authorize([Role.Staff]), (req, res) => {
-  if (clients.hasOwnProperty(req.body.receiverName)) {
-    clients[req.body.receiverName].emit("SCAN_QR_CODE_CONFIRM", req.body);
-    senders[req.user.id] = req.body;
-    clients[req.body.receiverName].userId = req.user.id;
+  console.log(req.body.receiverName, Object.keys(global.clients));
+  if (global.clients.hasOwnProperty(req.body.receiverName)) {
+    global.clients[req.body.receiverName].emit("SCAN_QR_CODE_CONFIRM", req.body);
+    global.senders[req.user.id] = req.body;
     res.status(201).send({ statusCode: 201, status: "success", message: "Paired Device " + req.body.receiverName });
   } else {
     res.status(404).send({ statusCode: 404, status: "fail", message: "Your partner " + req.body.receiverName + " was gone" });
@@ -21,7 +18,7 @@ router.post("/register", authorize([Role.Staff]), (req, res) => {
 });
 
 router.post("/check", authorize([Role.Staff]), (req, res) => {
-  if (senders[req.user.id] && clients.hasOwnProperty(senders[req.user.id].receiverName)) {
+  if (global.senders[req.user.id] && global.clients.hasOwnProperty(global.senders[req.user.id].receiverName)) {
     res.status(200).send({ statusCode: 200, status: "success", message: "Paired Device " + req.body.receiverName });
   } else {
     res.status(404).send({ statusCode: 404, status: "fail", message: "Your partner " + req.body.receiverName + " was gone" });
@@ -47,32 +44,35 @@ router.post("/timer/:type", authorize([Role.Staff]), (req, res) => {
       event = null;
   }
 
-  if (!senders[req.user.id]) {
+  if (!global.senders[req.user.id]) {
     return res.status(409).send({ statusCode: 409, status: "fail", message: "Please follow instructions to pair the device" });
   }
-  let receiverName = senders[req.user.id].receiverName;
-  if (clients.hasOwnProperty(receiverName)) {
-    clients[receiverName].emit(event, req.body);
+  let receiverName = global.senders[req.user.id].receiverName;
+  if (global.clients.hasOwnProperty(receiverName)) {
+    global.clients[receiverName].emit(event, req.body);
     res.status(201).send({ statusCode: 201, status: "success", message: "Message delivered" });
   } else {
     res.status(409).send({ statusCode: 409, status: "fail", message: "Your partner " + receiverName + " was gone" });
   }
 });
 router.post("/disconnect", authorize([Role.Staff]), (req, res) => {
-  if (senders.hasOwnProperty(req.user.id)) {
-    let receiverName = senders[req.user.id].receiverName;
-    if (clients.hasOwnProperty(receiverName)) {
-      clients[receiverName].emit("DISCONNECT_TIMER", {});
+  if (global.senders.hasOwnProperty(req.user.id)) {
+    let receiverName = global.senders[req.user.id].receiverName;
+    if (global.clients.hasOwnProperty(receiverName)) {
+      global.clients[receiverName].emit("DISCONNECT_TIMER", {});
     }
-    delete senders[req.user.id];
+    delete global.senders[req.user.id];
   }
 
-  res.status(200).send({ statusCode: 201, message: "Disconnected" });
+  res.status(200).send({ statusCode: 200, message: "Disconnected" });
 });
 
 module.exports = {
   router,
   init: (server) => {
+    console.log("Init websocket");
+    global.clients = {};
+    global.senders = {};
     io = SocketIO(server, {
       cors: {
         origin: "*",
@@ -81,18 +81,16 @@ module.exports = {
     });
     io.on("connection", (socket) => {
       socket.on("disconnect", () => {
-        if (socket.username) console.log("DEVICE_DISCONNECTED", socket.username);
-        delete clients[socket.username];
-        delete senders[socket.userId];
+        console.log("DEVICE_DISCONNECTED", socket.id, socket.username, socket.userId);
+        delete global.clients[socket.username];
+        delete global.senders[socket.userId];
       });
 
       socket.on("REGISTER_TIMER", (msg) => {
-        console.log("REGISTER_TIMER:", msg);
-        let username = msg.username;
-        socket.username = username;
-        clients[username] = socket;
-        console.log("REGISTER_TIMER:", { status: "success", message: "Socket registered" });
-        socket.emit("REGISTER_TIMER_RESP", { status: "success", message: "Socket registered" });
+        socket.username = msg.username;
+        global.clients[msg.username] = socket;
+        global.clients[msg.username].emit("REGISTER_TIMER_RESP", { status: "success", message: "Socket registered" });
+        console.log("REGISTER_TIMER_RESP:", { msg, status: "success", message: "Socket registered" });
       });
     });
   }
